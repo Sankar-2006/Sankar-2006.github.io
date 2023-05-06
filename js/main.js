1f336c8b36586282ac3593eeb0896807ec8b0df6
Sankar-2006 <umasankarg2006@gmail.com>
2023-05-06 17:48:33 +0530

    added responsive design

--- js/main.js
+++ js/main.js
@@ -4,10 +4,10 @@
 
 var typed = new Typed('#typingEffect', {
     strings: ['Uma Sankar.', 'Frontend Developer.'],
-    startDelay: 1500,
+    startDelay: 1600,
     typeSpeed: 100,
     backSpeed: 100,
-    backDelay: 1500,
+    backDelay: 1600,
     loop: true
 });
 
--- js/nav.js
+++ js/nav.js
@@ -1,14 +1,44 @@
+// this contains main elements 
+
 const nav = document.querySelectorAll('.nav');
 
-nav.forEach(e => {
-    e.addEventListener(('click'), () => {
-        removeActive();
-        e.classList.add('active');
-    });
-})
+const sections = document.querySelectorAll('section');
 
-function removeActive() {
-    nav.forEach((e) => {
-        e.classList.remove('active');
+window.onscroll = () => {
+    let active;
+    sections.forEach(section => {
+        let offset = section.offsetTop;
+        let height = section.clientHeight;
+        if (pageYOffset >= (offset - height/4)) {
+            active = section.getAttribute('id');
+        }
     })
+    nav.forEach(e => {
+        e.classList.remove('active');
+        if(e.classList.contains(active)) {
+            e.classList.add('active');
+        }
+    })
+};
+
+// nav icon
+
+let icon = document.getElementById('menu');
+let sidebar = document.getElementById('sideBar');
+
+icon.onclick = () => {
+    sidebar.classList.toggle('active');
+    icon.classList.toggle('active');
 }
+
+document.onclick = e => {
+    if (e.target.id !== 'sideBar' && e.target.id !== 'menu') {
+        navElement.classList.remove('active');
+    }
+}
+
+nav.forEach(e => {
+    e.onclick = () => {
+        navElement.classList.remove('active');
+    }
+})
